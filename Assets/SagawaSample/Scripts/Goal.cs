58dc505aaf2839a43fe9514170f3c0478f5272c5
freeman-as <freeman.by2018@gmail.com>
2023-03-03 09:56:06 +0900

    add 佐川作業用サンプルコード・シーン

---
+++ Assets/SagawaSample/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _particle;
+
+    void Start()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Finger")
+        {
+            Debug.Log("ÉSÅ[ÉãÇµÇ‹ÇµÇΩÅI");
+            _particle.SetActive(true);
+        }
+    }
+}
